6408a0cbfb29de4b5eb3c34228fcb0e26c051bd6
Areyesenc <55030875+Areyesenc@users.noreply.github.com>
2021-06-25 23:06:36 -0400

    Update TDA.pl
    up

--- TDA.pl
+++ TDA.pl
@@ -3,10 +3,16 @@ fecha(D,M,Y,F):- F = date(D,M,Y).
 socialNetwork(Name,Date,Sn2):- Sn2  = [Name,Date,[],0].
 
 socialNetworkRegister(Sn,Date,Username,Password,SnN):- getUsers(Sn,Users), append(Users,[[Date,Username,Password,[]]],New), updateUsers(Sn,New,SnN).   
+socialNetworkRegister(Sn,Username,_,Sn):- not(checkUser(Sn,Username)).   
+
 
 getUsers(Sn,Users):- Sn = [_,_,Users,_].
+
+checkUser(Sn,User):- getUsers(Sn,Users),not(member([User,_,_,_],Users)).
 
 updateUsers(Sn,New,SnN):- Sn = [N,D,_,L], SnN = [N,D,New,L].
 
 socialNetworkLogin(Sn1,Username,Password,Sn2):- getUsers(Sn1,Users),member([_,Username,Password,_],Users),Sn3 = [_,_,Users,1],updateUsers(Sn1,Sn3,Sn2).
+socialNetworkLogin(Sn1,Username,Password,Sn1):- getUsers(Sn1,Users),not(member([Username,Password,_,_],Users)).
 
+loggedin(Sn1,Username):- Sn1 = [_,_,_,Username].
